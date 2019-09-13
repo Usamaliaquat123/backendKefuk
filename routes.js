817241084caee1c549792cb0c6e74374a636c5c9
Usamaliaquat123 <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2019-09-13 08:57:51 +0500

    as

--- routes.js
+++ routes.js
@@ -11,24 +11,40 @@ router.get("/",(req,res,next) => {
 })
 const stripe = new stripeLoader("sk_test_Zq0gEeme3OrsfasvNj1usezK005wYl82oQ");
 
-const charge = (token, amt, email) => {
+const charge = (token, amt, email,descText) => {
   return stripe.charges.create({
-    amount: amnt * 100,
+    amount: amt * 100,
     currency: "usd",
     source: token,
-    description: "Statement Description",
-    receipt_email: email
+    description: descText,
+    receipt_email: email,
+  
   });
 };
+
+
 router.get("/api/donate", (req,res,next) => {
   res.send('Api of donate section')
 })
 router.post("/api/donate", async (req, res, next) => {
   // try {
     console.log(req);
-    // let data = await charge(req, req.body.token.id, req.body.amount);
-    console.log(req.body.token);
-    // res.send("Charged!");
+    let descText = ''
+    if(req.body.amount == 10){
+      descText = "Donation of education of a child"
+    }else if(req.body.amount == 25){
+      descText = "Sponsor a education of a child"
+    }else if(req.body.amount == 60){
+      descText = "Sponsor a female trainee teacher"
+    }else{
+      descText = "Custom Donation"
+    }
+    let data = await charge(req.body.token.id, req.body.amount, req.body.email, descText);
+    console.log("==================================");
+    console.log(data);
+    console.log("==================================");
+    res.send("Charged!");
+    
   // } catch (e) {
     // console.log(e);
     // res.send(500);
