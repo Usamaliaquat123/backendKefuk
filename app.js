97969a412ab01e8e627d071623dfc884c60938e9
Usamaliaquat123 <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2019-09-26 21:15:34 +0500

    asd

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 const apiRouter = require("./routes");
 const app = express();
-
-
+// const cors = require('cors')
 
 // let p = path.join(__dirname,'index.html')
 // console.log(p);
@@ -11,6 +10,12 @@ const app = express();
 // app.use(express.static(p))
 app.use(express.json());
 app.use(apiRouter);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const port = process.env.PORT || 5000;
 
